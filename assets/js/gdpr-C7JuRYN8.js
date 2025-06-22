faac2954652a2b7c57a9b133db980eb50ba3c09c
findforcebot <github@findforce.io>
2025-06-22 08:04:50 +0000

    chore: deploy 70d4628ce57f736368bcb8fa03f1d21a0af7ccad

---
+++ assets/js/gdpr-C7JuRYN8.js
@@ -0,0 +1,1 @@
+import"./styles-BCig7gsa.js";window.scrollToSignup=function(){var o=document.getElementById("signup");o?o.scrollIntoView({behavior:"smooth"}):window.location.href="/#signup"};
--- assets/js/terms-Cb4skKmS.js
+++
@@ -1,1 +0,0 @@
-import"./styles-BDTnQHA7.js";window.scrollToSignup=function(){var o=document.getElementById("signup");o?o.scrollIntoView({behavior:"smooth"}):window.location.href="/#signup"};
