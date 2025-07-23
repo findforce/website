e317228984d5b34ca7b39bc84bd037cd9528c368
findforcebot <github@findforce.io>
2025-07-23 12:48:19 +0000

    chore: deploy 622bba7ab9515919b32d170b248844afe1530a50

---
+++ assets/js/privacy-Dur1ZeJW.js
@@ -0,0 +1,1 @@
+import"./styles-CKF-vM9w.js";window.scrollToSignup=function(){var o=document.getElementById("signup");o?o.scrollIntoView({behavior:"smooth"}):window.location.href="/#signup"};
--- assets/js/privacy-M5aix8kH.js
+++
@@ -1,1 +0,0 @@
-import"./styles-DKjJ9N_v.js";window.scrollToSignup=function(){var o=document.getElementById("signup");o?o.scrollIntoView({behavior:"smooth"}):window.location.href="/#signup"};
