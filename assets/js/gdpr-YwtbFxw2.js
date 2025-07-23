c34b28ab716e116b9f184448b726ebab2bc02ff4
findforcebot <github@findforce.io>
2025-07-23 07:03:07 +0000

    chore: deploy 806341186177f665435bcc164e14963a38863f73

--- assets/js/gdpr-YwtbFxw2.js
+++
@@ -1,1 +0,0 @@
-import"./hubspot-Biq9Wh9N.js";/* empty css               */import"./vendor-Dyy25WtN.js";window.scrollToSignup=function(){var o=document.getElementById("signup");o?o.scrollIntoView({behavior:"smooth"}):window.location.href="/#signup"};
